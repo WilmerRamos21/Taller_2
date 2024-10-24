ad01dfc0a464dc0a736463e1cdf6748555c75d9e
Adrian <adrian_ramos@MacBook-Air-de-Adrian.local>
2024-10-24 07:44:24 -0500

    Initial commit

---
+++ src/Estudiantes.java
@@ -0,0 +1,13 @@
+public class Estudiantes {
+
+    //Atributos
+    String nombre;
+    String apellido;
+    String fechaNacimiento;
+    int edad;
+    int cedulaIdentidad;
+    int telefono;
+    double estatura;
+    double peso;
+
+}
